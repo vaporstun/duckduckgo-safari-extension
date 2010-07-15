2379c18eb3c122ffb135ba9481b9d06acdb1d951
Victor Quinn <vaporstun@gmail.com>
2010-07-15 13:54:47 -0400

    Added Ctrl-d as a shortcut to jump to search bar.

--- DuckDuckGoBar.safariextension/duckduckgo.js
+++ DuckDuckGoBar.safariextension/duckduckgo.js
@@ -6,3 +6,12 @@ function ddgHandler(ddgEvent) {
     sel = sel.replace(/^\s+|\s+$/g,"");
     safari.self.tab.setContextMenuEventUserInfo(ddgEvent, sel);
 }
+
+window.addEventListener('keydown', checkKeyDown, false);
+
+function checkKeyDown(e) {
+    if (e.which == 17) isCtrl=true;
+    if (e.which == 68 && isCtrl == true) {
+	safari.self.tab.dispatchMessage('ddgFocus');
+    }
+}
